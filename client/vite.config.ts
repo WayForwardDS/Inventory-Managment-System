0e78ab3bba98a988e72d3839b81f48e796812e40
Way Forward Digital Solutions <wayforwarddigitalsolutions@gmail.com>
2025-06-19 11:30:55 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,13 +19,13 @@
 //   // },
 // })
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For Netlify
   build: {
-    outDir: 'dist',
-  },
-});
+    outDir: 'dist', 
+    emptyOutDir: true 
+  }
+})
