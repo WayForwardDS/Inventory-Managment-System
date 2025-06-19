77eebba5391fd6f383f706de13a89c4ba65bfcb7
Way Forward Digital Solutions <wayforwarddigitalsolutions@gmail.com>
2025-06-19 11:05:24 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',  
+//     port: 5173,      
+//   },
+//   // build: {
+//   //   sourcemap: false, 
+//   //   minify: 'terser', 
+//   //   terserOptions: {
+//   //     compress: {
+//   //       drop_console: true,
+//   //       drop_debugger: true,
+//   //     },
+//   //   },
+//   // },
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  
-    port: 5173,      
+  base: '/', // For Netlify
+  build: {
+    outDir: 'dist',
   },
-  // build: {
-  //   sourcemap: false, 
-  //   minify: 'terser', 
-  //   terserOptions: {
-  //     compress: {
-  //       drop_console: true,
-  //       drop_debugger: true,
-  //     },
-  //   },
-  // },
-})
+});
