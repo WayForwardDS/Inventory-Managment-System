ce3d0f3e28e3b858e68e389f674108dfce318548
Way Forward Digital Solutions <wayforwarddigitalsolutions@gmail.com>
2025-06-19 15:26:48 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -25,7 +25,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', 
     outDir: '../dist',
     emptyOutDir: true 
   }
